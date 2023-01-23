3c77f5e710221a49adfc034811d2b881d643a8f4
Shuvo Sorkar <123367419+ShreevasBiswas@users.noreply.github.com>
2023-01-23 17:22:19 +0600

    Restorent project html amd css.

---
+++ index.js
@@ -0,0 +1,136 @@
+//document.write("")
+//alert("")
+//console.log("")
+//var number = 5.7382926;
+//console.log(number.toPrecision(5));
+//document.write()
+/*var x,y;
+x = "shuvo";
+y = 19;
+alert('my name is :'+x+y);*/
+//var num = "10.6858448440";
+//num = parseInt(num);
+//console.log(Number(false));
+
+/*var car;
+car = prompt("enter your name");
+y = car.length;
+alert("number of crt  :"+y);*/
+
+/*var m = "shuvo sorkar";
+//m = m.charAt(2);
+document.write(m.toUpperCase(m));*/
+
+/*var name1,name2
+name1 = prompt("enter your first name :");
+name2 = prompt("enter your last name");
+name = name1 + name2;
+lg = name.length;
+car = name.charAt(2);
+
+document.write(name.toUpperCase() +"number :"+ lg + "velu:" + car);*/
+
+/*var num = prompt("enter a number");
+if(num % 2 == 0)
+console.log("even");
+else
+console.log("odd");*/
+
+/*var marks = prompt("enter your marks");
+if(marks > 100 || marks < 0)
+document.write("invaild number");
+ else if(marks >= 80 && marks <= 100)
+document.write("A+");
+else if(marks >= 70 && marks <= 79)
+document.write("A");
+else if(marks >= 60 && marks <= 69)
+document.write("A-");
+else if(marks >= 50 && marks <= 59)
+document.write("B");
+else if(marks >= 40 && marks <= 49)
+document.write("C");
+else if(marks >= 30 && marks <= 39)
+document.write("D");
+else
+document.write("FAIL");*/
+
+/*var number1,number2,number3
+number1 = prompt("enter number1");
+number2 = prompt("enter number2");
+number3 = prompt("enter number3");
+if(number1>number2 && number1>number3)
+document.write(number1);
+
+else if(number2>number1 && number2>number3)
+document.write(number2);
+
+else
+document.write(number3);*/
+
+/*var letter
+letter = prompt("enter a letter");
+//letter = letter.tolowercase();
+if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+document.write("vowel");
+else
+document.write("consonant");*/
+
+//switch,case,break,default
+/*var letter = prompt("enter a letter");
+switch(letter){
+  case "1":
+    document.write("one");
+    break;
+ case "2":
+   document.write("two");
+   break;
+ default:
+ document.write("invalid");
+}*/
+
+/*var letter =prompt("enter a letter");
+switch(letter){
+  case "a":
+   // document.write("vowel");
+  //  break;
+    case "e":
+ //   document.write("vowel");
+//    break;
+    case "i":
+ //   document.write("vowel");
+//    break;
+    case "o":
+//    document.write("vowel");
+//    break;
+    case "u":
+    document.write("vowel");
+    break;
+  default:
+  document.write("consonant");
+}*/
+
+//starting,condition,update
+
+/*for (var x = 2; x <= 100; x = x +2 )
+{
+
+  document.write(" " + x);
+}*/
+/*for(var x=1; x<=1; x++){
+  var num1 = parseInt(prompt("enter first number"));
+var num2 = parseInt(prompt("enter last number"));
+sum = num1+num2
+}
+document.write(sum);*/
+
+/*var sum = 0
+var m = 1
+while(m <= 100){
+ if(m%3==0 && m%5==0){
+   document.write(" " + m);
+   sum = sum+m
+ }
+
+  m = m +1
+  }
+document.write(" " + sum);*/
